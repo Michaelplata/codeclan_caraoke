1b324868b918001566579222e39b9166393751bb
Michael <mikaelplata@gmail.com>
2021-10-28 22:16:09 +0100

    Add guest test has name

---
+++ tests/guest_test.py
@@ -0,0 +1,9 @@
+import unittest
+from classes.guest import Guest
+
+class TestGuest(unittest.TestCase):
+    def setUp(self):
+        self.guest = Guest("Donald Duck")
+    
+    def test_guest_has_name(self):
+        self.assertEqual("Donald Duck", self.guest.name)
