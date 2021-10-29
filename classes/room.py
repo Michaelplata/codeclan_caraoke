54508ef9718ce702f40a2146fd6d36d790029efd
Michael <mikaelplata@gmail.com>
2021-10-29 16:28:11 +0100

    Add tests can add songs and check in and out guests

--- classes/room.py
+++ classes/room.py
@@ -2,3 +2,23 @@ class Room:
     
     def __init__(self, theme):
         self.theme = theme
+        self.songs = []
+        self.guests = []
+    
+    def songs_count(self):
+        return len(self.songs)
+    
+    def add_song(self, new_song):
+        self.songs.append(new_song)
+    
+    def guests_count(self):
+        return len(self.guests)
+    
+    def check_in_guests(self, guest):
+        self.guests.append(guest)
+    
+    def check_out_guests(self, guest):
+        self.guests.remove(guest)
+
+
+
--- tests/room_test.py
+++ tests/room_test.py
@@ -1,15 +1,36 @@
 import unittest
 from classes.room import Room
+from classes.song import Song
+from classes.guest import Guest
 
 class TestRoom(unittest.TestCase):
     def setUp(self):
-        self.theme_1 = Room("Popstars")
-        self.theme_2 = Room("80's Night")
-        self.theme_3 = Room("Rock 'N' Roll")
-    
-    def test_room_has_theme(self):
-        self.assertEqual("Rock 'N' Roll", self.theme_3.theme)
+        self.room = Room("Rock 'N' Roll")
+        self.new_song_1 = Song("I will survive")
+        self.new_song_2 = Song("Do not worry, be happy")
+        self.new_guest_1 = Guest("Donald Duck") 
+        self.new_guest_2 = Guest("Daffy Duck")
         
+    def test_room_has_theme(self):
+        self.assertEqual("Rock 'N' Roll", self.room.theme)
+    
+    def test_can_add_songs(self):
+        self.room.add_song(self.new_song_1)
+        self.room.add_song(self.new_song_2)
+        self.assertEqual(2, self.room.songs_count())
+
+    def test_guest_can_check_in(self):
+        self.room.check_in_guests(self.new_guest_1)
+        self.room.check_in_guests(self.new_guest_2)
+        self.assertEqual(2, self.room.guests_count())
+
+    def test_guest_can_check_out(self):
+        self.room.check_in_guests(self.new_guest_1)
+        self.room.check_in_guests(self.new_guest_2)
+        self.room.check_out_guests(self.new_guest_1)
+        self.assertEqual(1, self.room.guests_count())
+
+
     
 
     
